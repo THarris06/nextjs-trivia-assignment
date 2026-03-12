d130643a18298cc53fd6b45391a5d96c0cf0faad
THarris06 <144939694+THarris06@users.noreply.github.com>
2026-03-12 10:10:04 -0230

    step 1

--- app/characters/[id]/page.jsx
+++ app/characters/[id]/page.jsx
@@ -1,10 +1,19 @@
 import Character from "@/components/Character.jsx"
-export default async function CharacterDetail( props ) {
-    const {id} = await props.params;
 
-    const response = await fetch("https://rickandmortyapi.com/api/character/" + id )
-    const data = await response.json()
+export async function generateStaticParams() {
+    const data = await fetch('https://rickandmortyapi.com/api/character')
+        .then((res) => res.json())
+
+    return data.results.map((character) => ({
+        id: character.id.toString(),
+    }))
+}
+
+export default async function CharacterDetail({ params }) {
+    const {id} = await params;
+    const character = await fetch('https://rickandmortyapi.com/api/character/' + id)
+        .then((res) => res.json());
     return (
-        <Character name={data.name} species={data.species} imageURL={data.image}/>
+        <Character name={character.name} species={character.species} imageURL={character.image}/>
     )
 }
--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     <>
       <h2>Rick and Morty Page</h2>
       <p>Welcome to the page, checkout some of the characters</p>
-      <Image alt="Eric" src="/Eric.jpg" width={200} height={200}></Image>
+      {/* <Image alt="Eric" src="/Eric.jpg" width={200} height={200}></Image> */}
     </>
   );
 }
